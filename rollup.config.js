14aafaf62f6a9391e2b9fa30dffc5b4bdfb94d56
Slava Cherkesov <cherkesov.vyacheslav@e-queo.com>
2024-02-07 20:32:12 +0100

    build: update config for build dts files

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,25 @@
 import typescript from '@rollup/plugin-typescript';
 import { dts } from 'rollup-plugin-dts';
 
-export default {
-  input: './index.ts',
-  output: [
-    {
-      file: './dist/index.cjs',
-      format: 'cjs',
-    },
-    {
-      file: './dist/index.mjs',
-      format: 'es',
-    },
-  ],
-  plugins: [typescript(), dts()],
-  external: ['vue-demi'],
-};
+export default [
+  {
+    input: './index.ts',
+    output: [
+      {
+        file: './dist/index.cjs',
+        format: 'cjs',
+      },
+      {
+        file: './dist/index.mjs',
+        format: 'es',
+      },
+    ],
+    plugins: [typescript()],
+    external: ['vue-demi'],
+  },
+  {
+    input: './index.ts',
+    output: [{ file: './dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
