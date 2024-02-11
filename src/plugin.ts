ae7e0f7a2f5737635753d542a7876f2a7059f237
Slava Cherkesov <cherkesov.vyacheslav@e-queo.com>
2024-02-11 18:30:02 +0100

    refactor: move config.ts -> constants.ts

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,27 +1,27 @@
 import { App } from 'vue-demi';
 import type { BemModsObject, BemPluginOptions } from './types';
 
-import { DEFAULT_DELIMITERS } from './config';
+import { DEFAULT_DELIMITERS } from './constants';
 
 import { generateModifiersFromObject } from './bem';
 
 import { DELIMITERS_INJECTION_KEY, NAMESPACE_INJECTION_KEY } from './use-bem';
 
 export const VueBem = {
-  install(app: App, options: BemPluginOptions) {
-    if (options.namespace) {
+  install(app: App, options?: BemPluginOptions) {
+    if (options?.namespace) {
       app.provide(NAMESPACE_INJECTION_KEY, options.namespace);
     }
 
-    const delimiters = options.delimiters
+    const delimiters = options?.delimiters
       ? { ...DEFAULT_DELIMITERS, ...options.delimiters }
       : DEFAULT_DELIMITERS;
 
-    if (options.delimiters) {
+    if (options?.delimiters) {
       app.provide(DELIMITERS_INJECTION_KEY, delimiters);
     }
 
-    const bemMethod = options.methodName ?? 'b';
+    const bemMethod = options?.methodName ?? 'b';
 
     app.config.globalProperties[bemMethod] = (
       element: string,
