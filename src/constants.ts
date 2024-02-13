6a0160c6050501b1368430361b945124f27b1eb6
Slava Cherkesov <cherkesov.vyacheslav@e-queo.com>
2024-02-13 21:46:02 +0100

    chore: move warning to constant

--- src/constants.ts
+++ src/constants.ts
@@ -9,4 +9,6 @@ export const DEFAULT_DELIMITERS: BemDelimiters = {
 
 export const ERROR_MESSAGES = {
   emptyBlock: '[vue-use-bem]: Block is not specified',
+  wrongModificatorType: (element: string, value: unknown) =>
+    `[vue-bem-cn]: Invalid modificator value type: ${typeof value} for element ${element}`,
 };
