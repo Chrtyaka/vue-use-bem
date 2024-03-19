f056f0fa3f98ea5b0d2df1fac4c7354a431accef
Slava Cherkesov <cherkesov.vyacheslav@e-queo.com>
2024-03-19 12:22:57 +0100

    fix(dts): fix build types. remove plugin dts

--- dist/types/src/constants.d.ts
+++
@@ -1,7 +0,0 @@
-import type { BemDelimiters } from './types';
-export declare const DEFAULT_DELIMITERS: BemDelimiters;
-export declare const ERROR_MESSAGES: {
-    emptyBlock: string;
-    wrongModificatorType: (element: string, value: unknown) => string;
-};
-export declare const BEM_METHOD_NAME = "b";
